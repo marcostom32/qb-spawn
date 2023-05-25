2d11e06fa295177cbdfd3916dfe674503ada84c6
X <110818161+KlovnDEV@users.noreply.github.com>
2023-05-25 04:15:47 +0200

    Updated and added locales

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # qb-spawn
-qb-spawn design
+qb-spawn design - updated for newest qbcore (Date of update: 25.05.2023)
+
+Updated by Metalholt#1942
 
 ![](https://cdn.discordapp.com/attachments/797581154315927573/913605989880692827/unknown.png)
 
-#qb-multicharacter 
+# FORK & CREDITS OF/TO
 https://github.com/marcostom32/qb-multicharacter
+
--- server.lua
+++ server.lua
@@ -1,14 +1,14 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-QBCore.Functions.CreateCallback('qb-spawn:server:getOwnedHouses', function(source, cb, cid)
+QBCore.Functions.CreateCallback('qb-spawn:server:getOwnedHouses', function(_, cb, cid)
     if cid ~= nil then
-        local houses = exports.oxmysql:executeSync('SELECT * FROM player_houses WHERE citizenid = ?', {cid})
+        local houses = MySQL.query.await('SELECT * FROM player_houses WHERE citizenid = ?', {cid})
         if houses[1] ~= nil then
             cb(houses)
         else
-            cb(nil)
+            cb({})
         end
     else
-        cb(nil)
+        cb({})
     end
 end)
